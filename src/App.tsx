b2c95be58051181af8331129aa4234849461a81f
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-10-26 19:03:12 -0300

    estudo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      olá mundo
+    </div>
+  );
+}
+
+export default App;
